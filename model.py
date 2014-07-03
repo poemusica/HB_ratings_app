a2d1197481952819841e97e1b38afbcac3556056
Hackbright Student <info@hackbrightacademy.com>
2014-07-02 17:34:20 -0700

    Completed Part 3 Ch1 (no bonus).

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+import correlation
 
 ENGINE = create_engine("sqlite:///ratings.db", echo=False)
 session = scoped_session(sessionmaker(bind=ENGINE,
@@ -21,6 +22,35 @@ class User(Base):
     zipcode = Column(String(15), nullable = True)
     email = Column(String(64), nullable = True)
     password = Column(String(64), nullable = True)
+
+    def similarity(self, other):
+        u_ratings = {}
+        paired_ratings = []
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        other_ratings = movie.ratings
+        similarities = [ (self.similarity(r.user), r) \
+            for r in other_ratings ]
+        similarities.sort(reverse=True)
+        similarities = [ s for s in similarities if s[0] > 0 ]
+        if not similarities:
+            return None
+        numerator = sum( [ r.rating * s for s, r in similarities ] )
+        denominator = sum( [ s[0] for s in similarities ] )
+     
+        return numerator / denominator
 
 
 class Movie(Base):
